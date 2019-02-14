9dbf391a2943ab9598305e65ddfa38e3a6c41a9f
Stqrm <hater2@mail.ru>
2019-02-14 18:07:46 +0300

    Keep values on POST

--- app.rb
+++ app.rb
@@ -28,17 +28,18 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
 post '/visit' do
 	
-	c = Client.new params[:client]
+	@c = Client.new params[:client]
 
-	if c.save
+	if @c.save
 		erb "<h2>Thank you! You signed!</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 	end
 end
